e99858344b75352162e685bf94c0f837d8040594
koson trachu <koson.trachu@gmail.com>
2018-10-24 03:15:12 +0700

    Example 1

---
+++ eventtest1/eventtest1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eventtest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Incrementer incrementer = new Incrementer();
+            Dozen dozensCounter = new Dozen(incrementer);
+            incrementer.DoCount();
+            Console.WriteLine("Number of Dozens = {0}", dozensCounter.DozensCount);
+        }
+    }
+
+    delegate void Handler();
+
+    class Incrementer
+    {
+        public event Handler CountedADozen;
+
+        public void DoCount()
+        {
+            for (int i = 0; i < 120; i++)
+            {
+                if (i % 12 == 0 && CountedADozen != null)
+                {
+                    CountedADozen();
+                }
+            }
+        }
+    }
+
+    class Dozen
+    {
+        public int DozensCount { get; private set; }
+        public Dozen(Incrementer incrementer)
+        {
+            DozensCount = 0;
+            incrementer.CountedADozen += IncrementDozensCount;
+
+        }
+
+        private void IncrementDozensCount()
+        {
+            DozensCount++;
+            Console.WriteLine("Increment dozen: result = {0}", DozensCount);
+        }
+    }
+
+
+}
